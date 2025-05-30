82570ab2344cd4511bc9bf2bff874b07d7ed2547
Ihor Leonov <ihorleonow@gmail.com>
2025-05-30 13:22:18 +0300

    Update meta

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,9 +12,35 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
+const appUrl = "https://xlsx-json-converter.vercel.app";
+
 export const metadata: Metadata = {
   title: "XLSX to JSON converter",
   description: "Online XLSX to JSON Converter",
+  applicationName: "XLSX to JSON Converter",
+  keywords: [
+    "XLSX to JSON",
+    "XLSX converter",
+    "JSON converter",
+    "XLSX to JSON online",
+    "convert XLSX to JSON",
+    "xlsx to json",
+    "xlsx converter",
+    "json converter",
+    "xlsx to json online",
+    "convert xlsx to json",
+    "xlsx json converter",
+    "xlsx to json converter",
+    "xlsx to json online converter",
+    "xlsx to json file",
+  ],
+  alternates: {
+    canonical: appUrl,
+    languages: {
+      en: appUrl,
+      "x-default": appUrl,
+    },
+  },
 };
 
 export default function RootLayout({
